c3a4309ba3ef5b4d5abdd47c5b311065ae09e3bf
Alikhann <alikhannugmanov@gmail.com>
2014-03-03 02:14:56 +0600

    Calculate Vertex Normals

--- WindowsFormsApplication1/Triangles.cs
+++ WindowsFormsApplication1/Triangles.cs
@@ -11,28 +11,105 @@ namespace WindowsFormsApplication1
 {
     class Triangles
     {
-        public Triangles() { }
+        
         Vector3 a, b, c;
+        Vector3 u, v;
 
+        int vertexID;
+        int indexID;
+        int normalID;
+
+        Vector3[] vertices;
+        uint[] indices;
+        Vector3[] normals;
+
+        public Triangles() 
+        {
+            vertices = new Vector3[]
+            {
+                new Vector3(0.0f, 8.0f, 15.0f),
+                new Vector3(15.0f, 8.0f, 0.0f),
+                new Vector3(0.0f, 15.0f, 0.0f),
+                new Vector3(15.0f, 8.0f, -15.0f),
+                new Vector3(-15.0f, 8.0f, 0.0f)
+            };
+            indices = new uint[]
+            {
+                0, 1, 2,
+                1, 3, 2, 
+                4, 0, 2,
+                3, 4, 2
+            };
+            normals = new Vector3[vertices.Length];
+
+            for (int i = 0; i < indices.Length; i += 3)
+            {
+                Vector3 v0 = vertices[indices[i]];
+                Vector3 v1 = vertices[indices[i + 1]];
+                Vector3 v2 = vertices[indices[i + 2]];
+
+                Vector3 normal = Vector3.Normalize(Vector3.Cross(v2 - v0, v1 - v0));
+                normals[indices[i]] += normal;
+                normals[indices[i + 1]] += normal;
+                normals[indices[i + 2]] += normal;
+
+            }
+            for (int i = 0; i < vertices.Length; i ++)
+            {
+                normals[i] = Vector3.Normalize(normals[i]) * (-2);
+            }
+
+            GL.GenBuffers(1, out vertexID);
+            GL.BindBuffer(BufferTarget.ArrayBuffer, vertexID);
+            GL.BufferData(BufferTarget.ArrayBuffer, new IntPtr(vertices.Length * Vector3.SizeInBytes), vertices, BufferUsageHint.StaticDraw);
+
+            GL.GenBuffers(1, out indexID);
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, indexID);
+            GL.BufferData(BufferTarget.ElementArrayBuffer, new IntPtr(indices.Length * sizeof(uint)), indices, BufferUsageHint.StaticDraw);
+
+            GL.GenBuffers(1, out normalID);
+            GL.BindBuffer(BufferTarget.ArrayBuffer, normalID);
+            GL.BufferData(BufferTarget.ArrayBuffer, new IntPtr(normals.Length * Vector3.SizeInBytes), normals, BufferUsageHint.StaticDraw);
+
+        }
+      
         public void draw()
         {
-            GL.Begin(PrimitiveType.Triangles);
-            GL.Color3(Color.Green);
+            //GL.Begin(PrimitiveType.Triangles);
+            //GL.Color3(Color.Green);
 
-            GL.Vertex3(0, 8, 15);
-            GL.Vertex3(15, 8, 0);
-            GL.Vertex3(0, 15, 0);
+            //GL.Vertex3(0, 8, 15);   //v1
+            //GL.Vertex3(15, 8, 0);   //v2
+            //GL.Vertex3(0, 15, 0);   //v3
             //GL.Vertex3(15, 8, -15);
             //GL.Vertex3(-15, 8, 0);
-            GL.End();
+            //GL.End();
+            GL.BindBuffer(BufferTarget.ArrayBuffer, vertexID);
+            GL.VertexPointer(3, VertexPointerType.Float, 0, 0);
+            GL.EnableClientState(ArrayCap.VertexArray);
+
+            GL.BindBuffer(BufferTarget.ArrayBuffer, normalID);
+            GL.NormalPointer(NormalPointerType.Float, Vector3.SizeInBytes, 0);
+            GL.EnableClientState(ArrayCap.NormalArray);
+
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, indexID);
+            GL.DrawElements(PrimitiveType.Triangles, indices.Length, DrawElementsType.UnsignedInt, 0);
+            GL.Color3(Color.White);
+            GL.LineWidth(2f);
+            GL.DrawElements(PrimitiveType.LineStrip, indices.Length, DrawElementsType.UnsignedInt, 0);
+            //GL.DrawArrays(PrimitiveType.Triangles, 0, vertices.Length);
+
+           
          }
-        public Vector3 Normal
-        {
-            get
-            {
-                var dir = Vector3.Cross(b-a, c-a);
-                return dir;
-            }
-        }
+       
+        //public Vector3 Normal
+        //{
+        //    get
+        //    {
+        //        var dir = Vector3.Cross(b-a, c-a);
+                
+        //        return dir;
+        //    }
+        //}
     }
 }
